bba6d5832d254bc334278f66b63cc768a8af4b20
cagatay246 <cagataykilgi7@gmail.com>
2025-01-13 11:54:52 +0300

    1

---
+++ Kutuphane/Kutuphane/Kitap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane
+{
+    class Kitap
+    { 
+        public string kitapAdi { get; set; }
+        public string YazarAdi { get; set; }
+        public int SayfaSayisi { get; set; }
+        public string Yayinevi { get; set; }
+        public DateTime KayitTarihi { get; set; }
+
+        public Kitap()
+        {
+            KayitTarihi = DateTime.Now; 
+        }
+
+        public Kitap(string kitapAdi,string yazarAdi, int sayfaSayisi, string yayinevi ): this()
+        {
+            kitapAdi = kitapAdi;
+            SayfaSayisi = sayfaSayisi;
+            Yayinevi = yayinevi;
+            YazarAdi = yazarAdi;
+
+        }
+        public void BilgileriYazdir ()
+        {
+            Console.WriteLine($"Kitap adı: {kitapAdi}, Yazar adı: {YazarAdi}, Yayınevi: {Yayinevi}, Sayfa sayısı {SayfaSayisi} Kayıt Tarihi: {KayitTarihi.ToShortDateString()}");
+        }
+
+
+
+
+    }
+}
---
+++ Kutuphane/Kutuphane/Program.cs
@@ -0,0 +1,22 @@
+﻿using Kutuphane;
+
+class Program
+{
+    static void Main()
+    {
+        Kitap kitap1 = new Kitap("aylin","ayşe kulin",398,"remzi kitabevi");
+        Kitap kitap2 = new Kitap();
+
+        Console.WriteLine("\nkitap1 Bilgileri:");
+        kitap1.BilgileriYazdir();
+
+        Console.WriteLine("\nkitap2 Bilgileri:");
+        kitap2.BilgileriYazdir();
+
+
+
+
+
+
+    }
+}
